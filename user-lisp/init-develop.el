447af9ea55df51c735c0b58d98022866c17c64ac
Wenping Guo <ybyygu@gmail.com>
2022-10-17 21:11:11 +0800

    改进 magit 设置: 进入 commit 界面会自动切换 insert mode

--- user-lisp/init-develop.el
+++ user-lisp/init-develop.el
@@ -1,3 +1,15 @@
+;; [[file:../gwp-scratch.note::24325443][24325443]]
+(use-package with-editor)
+
+(add-hook 'shell-mode-hook  'with-editor-export-editor)
+(add-hook 'eshell-mode-hook 'with-editor-export-editor)
+(add-hook 'term-exec-hook   'with-editor-export-editor)
+(add-hook 'vterm-mode-hook  'with-editor-export-editor)
+
+;; 进入 insert 编辑模式
+(add-hook 'with-editor-mode-hook 'meow-insert-mode)
+;; 24325443 ends here
+
 ;; [[file:../gwp-scratch.note::8970c514][8970c514]]
 (general-define-key :prefix-map 'gwp::magit-map)
 
@@ -5,6 +17,13 @@
   :demand t
   :config
   (setq magit-completing-read-function 'ivy-completing-read)
+  ;; 隐藏untracked文件列表. 更多时候的操作是stage/commit
+  (setq magit-section-initial-visibility-alist (quote ((untracked . hide))))
+  ;;禁用magit中的gravatars支持, 响应能快一些.
+  (setq magit-revision-show-gravatars nil)
+  ;; 进入 magit-status 后, 将光标定在 unstaged 一栏
+  (setq magit-status-initial-section '(2))
+
   :bind
   (:map gwp::magit-map
    ;; ("j" . magit-next-line)
