a256cb25bce2340a169de950b3320387b45cbfc5
Wenping Guo <ybyygu@gmail.com>
2026-02-04 11:34:17 +0800

    yadm: 修复 commit 报错问题

--- user-lisp/yadm.el
+++ user-lisp/yadm.el
@@ -1,18 +1,20 @@
 ;; [[file:../gwp-scratch.note::840c0c02][840c0c02]]
 ;; https://www.reddit.com/r/emacs/comments/gjukb3/yadm_magit/
+;; https://github.com/TheLocehiliosan/yadm/blob/master/yadm.md#commands
 (use-package tramp
   :ensure nil
   :config
-  ;; (setenv "SHELL" (executable-find "bash"))
-  (unless (assoc-default "yadm" tramp-methods)
-    ;; https://github.com/TheLocehiliosan/yadm/blob/master/yadm.md#commands
-    (add-to-list 'tramp-methods
-                 '("yadm"
-                   (tramp-login-program "yadm")
-                   (tramp-login-args (("enter")))
-                   (tramp-login-env (("SHELL") ("/bin/sh")))
-                   (tramp-remote-shell "/bin/sh")
-                   (tramp-remote-shell-args ("-c"))))))
+  ;; [2026-02-04 Wed] 修复 yadm commit 时报错问题
+  (setq tramp-allow-unsafe-temporary-files t)
+  (setq magit-process-finish-refresh-remote nil)
+
+  (add-to-list 'tramp-methods
+               '("yadm"
+                 (tramp-login-program "yadm")
+                 (tramp-login-args (("enter")))
+                 (tramp-login-env (("SHELL") ("/bin/sh")))
+                 (tramp-remote-shell "/bin/sh")
+                 (tramp-remote-shell-args ("-c")))))
 
 (defun yadm--files ()
   (let ((default-directory "~/"))
@@ -31,18 +33,6 @@
 (defun yadm-status ()
   (interactive)
   (with-current-buffer (magit-status "/yadm::")))
-
-;; ;;;###autoload
-;; (defun yadm-stage ()
-;;   (interactive)
-;;   (let ((file
-;;          (let ((default-directory "~/"))
-;;            (read-file-name "Stage file: "))))
-;;     (if (equal (expand-file-name file)
-;;                (expand-file-name "~/.yadm/"))
-;;         (user-error "Can't stage yadm dir itself.")
-;;       (magit-with-toplevel
-;;         (magit-stage-1 nil (list file))))))
 
 ;;;###autoload
 (defun yadm-add-file ()
